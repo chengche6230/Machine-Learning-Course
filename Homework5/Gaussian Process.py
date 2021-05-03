22589c511dc5fc5ba4c541ac2be6906281b69040
Cheng Che <chengche6230@gmail.com>
2021-05-04 01:03:16 +0800

    done loadData

--- Homework5/Gaussian Process.py
+++ Homework5/Gaussian Process.py
@@ -6,6 +6,15 @@ Machine Learning Homework 5
 import numpy as np
 import matplotlib.pyplot as plt
 
+def loadData():
+    X = []
+    Y = []
+    with open("./data/input.data", 'r') as file:
+        for line in file.readlines():
+            li = line.split()
+            X.append(float(li[0]))
+            Y.append(float(li[1]))
+    return X, Y
 
 if __name__ == "__main__":
-    1
+    X, Y = loadData()
