5aac9e324386a00969a9de6b895887224f89e7b6
Cheng Che <chengche6230@gmail.com>
2021-05-30 01:16:05 +0800

    Kernel k means done

--- Homework6/Kernel K Means.py
+++ Homework6/Kernel K Means.py
@@ -3,38 +3,104 @@ Machine Learning Homework 6
     Kernel K Means
 
 """
+import os
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from matplotlib import image
 from tqdm import trange
 
+IMAGE = 1  # {1|2}
 img_length = 100
 img_size = 10000
-K = 2
+
+K = 2  # {2|3|4}
 gamma_s = 1 / img_size
 gamma_c = 1 / (256 * 256)
 kernel = None
 
-COLOR_2 = [[153, 102, 51], [0, 153, 255]]
-#COLOR_3 = 
-#COLOR_4 = 
+METHOD = "random"  # {random|kmenas++|naive_sharding}
 
-#%%
+COLOR = [[[153, 102, 51], [0, 153, 255]],
+         [[0, 102, 204], [51, 204, 204], [153, 102, 51]],
+         [[0, 102, 204], [51, 204, 204], [153, 102, 51], [153, 153, 153]]]
 
 def readImg(fileName):
     img = cv2.imread('./data/' + fileName)
     return img.reshape(img_size, 3)
 
+def minDist(m, pos):
+    minDis = 1e8
+    x = pos % img_length
+    y = pos // img_length
+    # Use spatial distance
+    for i in range(len(m)):
+        m_x = m[i][0] % img_length
+        m_y = m[i][0] // img_length
+        minDis = min(minDis, (x - m_x) ** 2 + (y - m_y) ** 2)
+    return minDis
+
+def nextMean(probSum):
+    num = np.random.rand()
+    for i in range(1, img_size):
+        if num >= probSum[i - 1] and num < probSum[i]:
+            return i
+
+def attrSum(img):
+    _sum = np.zeros((img_size, 5), dtype=np.float32)
+    # Five columns are {index|summation|color R|color G|color B}
+    for n in range(img_size):
+        # Sum up values of all attribute
+        _sum[n][0] = n
+        _sum[n][1] = (n % img_length) + (n // img_length)
+        for c in range(3):
+            _sum[n][c + 2] = img[n][c]
+            _sum[n][1] += img[n][c]
+    _sum = _sum[np.argsort(_sum[:, 1])]  # Sort by summation value
+    return _sum
+
+def sliceMean(_sum):
+    mIndex = int(np.average(_sum[:, 0]))
+    mR = int(np.average(_sum[:, 2]))
+    mG = int(np.average(_sum[:, 3]))
+    mB = int(np.average(_sum[:, 4]))
+    tmp_img = np.array([mR, mG, mB], dtype=np.uint8)
+    return [mIndex, tmp_img]
+
 def init(img):
     m = []
-    select = np.random.choice(img_size, K, replace=False)
-    for k in range(K):
-        m.append([select[k], img[select[k]]])
-    c = np.array([1 for k in range(K)])
+    
+    if METHOD == "random":
+        select = np.random.choice(img_size, K, replace=False)
+        for k in range(K):
+            m.append([select[k], img[select[k]]])
+            
+    if METHOD == "kmeans++":
+        t = np.random.randint(img_size)
+        m.append([t, img[t]])
+        for k in range(K-1):
+            distSum = 0
+            probSum = np.zeros(img_size, dtype=np.float32)
+            for n in range(img_size):
+                probSum[n] = minDist(m, n) # shortest distance to mean
+                distSum += probSum[n]
+            probSum[0] /= distSum # Normalize as probability
+            for n in range(1, img_size):
+                probSum[n] /= distSum
+                probSum[n] += probSum[n - 1] # Compute cumulative probability
+            m_index = nextMean(probSum)
+            m.append([m_index, img[m_index]])
+    
+    if METHOD == "naive_sharding":
+        sum_attr = attrSum(img)
+        _slice = img_size // K
+        for k in range(K):
+            slice_sum = sum_attr[k*_slice : (k+1)*_slice]
+            m.append(sliceMean(slice_sum))
+
     alpha = np.zeros((img_size, K), dtype=np.uint8)
     for k in range(K):
         alpha[m[k][0]][k] = 1
+    c = updateCk(alpha)
     return m, c, alpha
 
 def spatialSimi(p1, p2):
@@ -43,10 +109,9 @@ def spatialSimi(p1, p2):
     return p
 
 def colorSimi(c1, c2):
-    c = 0
-    for i in range(3):
-        c += (c1[i] - c2[i]) ** 2
-    return c
+    c1 = np.array(c1, dtype=np.uint32)
+    c2 = np.array(c2, dtype=np.uint32)
+    return np.sum((c1 - c2) ** 2)
 
 def k(x1, x2):
     s = np.exp(-gamma_s * spatialSimi(x1[0], x2[0]))
@@ -60,42 +125,35 @@ def computeKernel(img):
             kernel[p][q] = kernel[q][p] = k([p, img[p]], [q, img[q]])
     return kernel
 
-def distance(x, img, k, c, alpha, dist3rd):
-    d = kernel[x[0]][x[0]]
-    tmp = 0
-    for n in range(img_size):
-        if alpha[n][k] == 0:
-            continue
-        tmp += alpha[n][k] * kernel[x[0]][n]
-    d -= (2 / c[k]) * tmp
-    d += dist3rd[k]
-    return d
-
-def dist_third_term(alpha, c):
-    v = np.zeros(K)
+def distance(c, alpha):
+    dist = np.ones((K, img_size))
+    
     for k in range(K):
-        tmp = 0
-        for p in range(img_size):
-            if alpha[p][k] == 0:
-                continue
-            for q in range(img_size):
-                if alpha[q][k] == 0:
-                    continue
-                tmp += alpha[p][k] * alpha[q][k] * kernel[p][q]
-        v[k] = tmp / (c[k] ** 2)
-    print("Distance third term calculated.")
-    return v
+        a = alpha[:, k].reshape(-1, 1).T
+        second_term = a @ kernel
+        second_term = second_term * 2 / c[k]
+        dist[k] -= second_term.flatten()
+        
+        indicator = alpha[:, k].reshape(-1,1)
+        third_term = np.sum(indicator.T @ kernel @ indicator)
+        third_term /= (c[k] ** 2)
+        dist[k] += third_term
+    
+    return dist    
 
 def E_Step(img, c, alpha):
     a = np.zeros((img_size, K), dtype=np.uint8)
-    dist3rd = dist_third_term(alpha, c)
-    for n in trange(img_size):
+    
+    # Distance between all data points
+    dist = distance(c, alpha)
+    
+    # Clustering
+    for n in range(img_size):
         min_dist = 1e8
         min_k = None
         for k in range(K):
-            d = distance([n, img[n]], img, k, c, alpha, dist3rd) 
-            if d < min_dist:
-                min_dist = d
+            if dist[k][n] < min_dist:
+                min_dist = dist[k][n]
                 min_k = k
         if min_k is not None:
             a[n][min_k] = 1
@@ -119,7 +177,7 @@ def visualize(img, alpha):
     for n in range(img_size):
         for k in range(K):
             if alpha[n][k] == 1:
-                im[n//img_length][n%img_length] = COLOR_2[k]
+                im[n//img_length][n%img_length] = COLOR[K - 2][k]
     
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_subplot(1, 2, 1)
@@ -130,26 +188,31 @@ def visualize(img, alpha):
     plt.show()
     return im
 
-#%%
 if __name__ == "__main__":
-    img = readImg('image2.png')
+    
+    img = readImg(f'image{IMAGE}.png')
     pre_alpha = None
     delta = img_size
     _iter = 0
     
-    # Init mean
+    # Init mean and cluster assignment
     means, c, alpha = init(img)
     
-    kernel = computeKernel(img)
+    #kernel = computeKernel(img)
+    kernel = np.load(f'kernel_img{IMAGE}.npy')
     
-    while delta > 100:
+    result_file_path = f'./kkm result/img{IMAGE}_{K} class_{METHOD}'
+    try:
+        os.mkdir(result_file_path)
+    except:
+        pass
+    
+    while delta > 0:
         _iter += 1
         
         # E step: Classify all samples
         pre_alpha = alpha.copy()
-        print("Before E step")
         alpha = E_Step(img, c, alpha)
-        print("After E step")
         c = updateCk(alpha)
         
         # M step: re-compute means
@@ -158,7 +221,7 @@ if __name__ == "__main__":
         delta = computeChange(pre_alpha, alpha)
         print(f"Iter:{_iter}, delta:{delta}")
         
-        # Visualize cluster result
+        # Visualize and save clustering result
         result = visualize(img, alpha)
-        cv2.imwrite(f'./kkm result/img2_0527_1300/img2_kkm_{_iter}.jpg', result)
+        cv2.imwrite(f'{result_file_path}/{_iter}.jpg', result)
         
